a3507a126274a06ef058923daf94b8edee40cc81
sbx_user1051 <sbx_user1051@169.254.220.21>
2022-05-16 18:51:42 +0000

    Update requested for Proton - component/isolated-component

---
+++ sample-env-1/components/isolated-component/empty-component.tf
@@ -0,0 +1,9 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:279245132597:component/isolated-component
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+# Here are some comments so it doesn't complain
