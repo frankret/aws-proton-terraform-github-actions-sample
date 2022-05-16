8bf5d18d5eaeedfef7c808020f0001c668190a67
sbx_user1051 <sbx_user1051@169.254.220.21>
2022-05-16 18:57:21 +0000

    Update requested for Proton - component/isolated-component

---
+++ sample-env-1/components/isolated-component/empty-component.tf
@@ -0,0 +1,15 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:279245132597:component/isolated-component
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+# Here are some comments so it doesn't complain
+
+resource "aws_ssm_parameter" "component-test" {
+  name  = "component-test"
+  type  = "String"
+  value = "my-test-value"
+}
